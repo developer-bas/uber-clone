08a05b752b3e976d67fdca1be040146c7d49b8bb
Sebastian <cruzguzmaneric@gmail.com>
2020-11-20 05:26:38 -0600

    Adding animation to pickup viewcontroller

--- Uber/Controller/PickupController.swift
+++ Uber/Controller/PickupController.swift
@@ -19,6 +19,18 @@ class PickupController: UIViewController {
     let trip : Trip
     weak var delegate : PickupControllerDelegate?
     
+    private lazy var circularProgressView : CircularProgressView = {
+        let frame = CGRect(x: 0, y: 0, width: 360, height: 360)
+        let cpview = CircularProgressView(frame: .zero)
+        
+        cpview.addSubview(mapView)
+        mapView.setDimensions(height: 268, width: 268)
+        mapView.layer.cornerRadius = 268/2
+        mapView.centerX(inView: cpview)
+        mapView.centerY(inView: cpview, constant: 32)
+        return cpview
+    }()
+    
     private let cancelButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "cancel").withRenderingMode(.alwaysOriginal), for: .normal)
@@ -43,6 +55,7 @@ class PickupController: UIViewController {
         button.addTarget(self, action: #selector(handleAcceptTrip), for: .touchUpInside)
         return button
     }()
+    
 //    MARK: - Lifecycle
     init(trip: Trip){
         self.trip = trip
@@ -55,11 +68,9 @@ class PickupController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         configureUI()
         configureMapView()
-        
+        self.perform(#selector(animateProgress),with: nil, afterDelay: 0.5)
     }
     
     override var prefersStatusBarHidden: Bool{
@@ -69,9 +80,15 @@ class PickupController: UIViewController {
 //    MARK: - Selectors
     
     @objc func handleAcceptTrip(){
-        DriverService.shared.acceptTrip(trip: trip) { (error, ref) in
+        DriverService.shared.acceptTrip(trip: trip){ (error, ref) in
             self.delegate?.didAcceptTrip(self.trip)
-            
+        }
+    }
+    
+    @objc func animateProgress(){
+        circularProgressView.animatePulsatingLayer()
+        circularProgressView.setProgressWithAnimation(duration: 5, value: 0) {
+            self.dismiss(animated: true, completion: nil)
         }
     }
     
@@ -84,33 +101,34 @@ class PickupController: UIViewController {
     
     
     func configureMapView(){
+        
         let region = MKCoordinateRegion(center: trip.pickupCoordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
         mapView.setRegion(region, animated: false)
        
         mapView.addAndSelectAnnotation(forCoordinate: trip.pickupCoordinates)
     }
     
     func configureUI(){
-        view.backgroundColor = .backgroundColor
-        view.addSubview(cancelButton)
         
+        view.backgroundColor = .backgroundColor
+       
+       
+        view.addSubview(pickupLabel)
+        pickupLabel.centerX(inView: view)
+        pickupLabel.anchor(top: circularProgressView.bottomAnchor, paddingTop: 32)
+        
+        view.addSubview(cancelButton)
         cancelButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
         
         
-        view.addSubview(mapView)
-        mapView.setDimensions(height: 270, width: 270)
-        mapView.layer.cornerRadius = 270/2
-        mapView.centerX(inView: view)
-        mapView.centerY(inView: view, constant: -152 )
         
-        view.addSubview(pickupLabel)
-        pickupLabel.centerX(inView: view)
-        pickupLabel.anchor(top: mapView.bottomAnchor, paddingTop: 14)
+        view.addSubview(circularProgressView)
+        circularProgressView.setDimensions(height: 360, width: 360)
+        circularProgressView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        circularProgressView.centerX(inView: view)
         
         view.addSubview(acceptTripButton)
         acceptTripButton.anchor(top: pickupLabel.bottomAnchor,left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 32, paddingRight: 32,height: 50)
-        
-        
     }
-    
 }
--- Uber/Views/CircularProgressView.swift
+++ Uber/Views/CircularProgressView.swift
@@ -21,11 +21,14 @@ class CircularProgressView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureCircleLayers()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
     
 //    MARK: - Helper Functions
     
@@ -57,6 +60,32 @@ class CircularProgressView: UIView {
         return layer
     }
     
+    func animatePulsatingLayer(){
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.25
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    func setProgressWithAnimation(duration: TimeInterval, value: Float,
+                                  completion: @escaping()->Void ){
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 1
+        animation.toValue = value
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        progressLayer.strokeEnd = CGFloat(value)
+        progressLayer.add(animation,forKey: "animateProgress")
+        
+        CATransaction.commit()
+        
+    }
     
 }
 
